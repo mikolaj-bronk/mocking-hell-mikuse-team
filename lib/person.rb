9f53d39b050879f59546348e38bd06ad1b219ee7
Seweryn Rutkowski <sakasew@gmail.com>
2018-01-05 15:56:13 +0100

    Added person.rb

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  attr_accessor :id, :firstname, :lastname, :country
+
+  def initialize(id, firstname, lastname, country)
+    @id = id
+    @firstname = firstname
+    @lastname = lastname
+    @country = country
+  end
+
+  def to_s
+    person = "#{firstname} #{lastname}, #{country}"
+    person
+  end
+end
