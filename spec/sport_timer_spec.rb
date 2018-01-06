6862755fab9a8bffe4ac03f31d71d3cf2ff06a22
Dominik Ryńko <dominikrynko@gmail.com>
2018-01-06 18:01:57 +0100

    Space correction

--- spec/sport_timer_spec.rb
+++ spec/sport_timer_spec.rb
@@ -2,9 +2,9 @@ require_relative '../lib/sport_timer.rb'
 require_relative '../lib/person.rb'
 
 RSpec.describe 'SportTimer' do
-   subject(:sport_timer) { SportTimer.new }
+  subject(:sport_timer) { SportTimer.new }
 
-   context '#add_person' do
+  context '#add_person' do
     it {
       pers = double('Person', firstname: 'Jan', lastname: 'Bak', country: 'Poland')
       expect { sport_timer.add_person(pers.firstname, pers.lastname, pers.country) }.not_to raise_error
@@ -25,34 +25,33 @@ RSpec.describe 'SportTimer' do
   end
 
   context '#edit_person' do
-      it {
-        country = 'Germany'
-        expect { sport_timer.edit_person(0, '', '', country) }.not_to raise_error
-      }
-      it {
-        firstname = 'Math'
-        lastname = 'Janniston'
-        expect { sport_timer.edit_person(0, firstname, lastname, '') }.not_to raise_error
-      }
-      it {
-        firstname = 'Math'
-        lastname = 'Janniston'
-        country = 'France'
-        expect { sport_timer.edit_person(0, firstname, lastname, country) }.not_to raise_error
-      }
-      it {
-        expect { sport_timer.edit_person(1, '', '', '') }.not_to raise_error
-      }
-    end
+    it {
+      country = 'Germany'
+      expect { sport_timer.edit_person(0, '', '', country) }.not_to raise_error
+    }
+    it {
+      firstname = 'Math'
+      lastname = 'Janniston'
+      expect { sport_timer.edit_person(0, firstname, lastname, '') }.not_to raise_error
+    }
+    it {
+      firstname = 'Math'
+      lastname = 'Janniston'
+      country = 'France'
+      expect { sport_timer.edit_person(0, firstname, lastname, country) }.not_to raise_error
+    }
+    it {
+      expect { sport_timer.edit_person(1, '', '', '') }.not_to raise_error
+    }
+  end
 
   context '#remove_person' do
-      it { expect { sport_timer.remove_person(0) }.not_to raise_error }
-      it { expect { sport_timer.remove_person(1) }.not_to raise_error }
-    end
+    it { expect { sport_timer.remove_person(0) }.not_to raise_error }
+    it { expect { sport_timer.remove_person(1) }.not_to raise_error }
+  end
 
   context '#show_person' do
     it { expect { sport_timer.show_person(0) }.not_to raise_error }
     it { expect { sport_timer.show_person(1) }.not_to raise_error }
   end
-
 end
