ec2c49d4278d6ada2558c64c2d3ee8340eec94a2
Dominik Ryńko <dominikrynko@gmail.com>
2018-01-06 17:20:34 +0100

    Program menu init

---
+++ lib/program.rb
@@ -0,0 +1,22 @@
+class Program
+  def initialize
+    menu
+  end
+
+  def menu
+    system 'clear'
+    puts '------------------------------------------------'
+    puts '|                                              |'
+    puts '|                Sport Timer                   |'
+    puts '|                                              |'
+    puts '-----------------------------------------------'
+  end
+
+  def authors
+    puts 'Autorzy: '
+    puts 'Jakub Ronkiewicz'
+    puts 'Seweryn Rutkowski'
+    puts 'Dominik Ryńko'
+    puts 'Mikołaj Bronk '
+  end
+end
